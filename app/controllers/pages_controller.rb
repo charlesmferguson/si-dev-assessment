d94ec89cd83741d4898304205383fc37db5f319a
Charles Ferguson <charles.m.ferguson.jr@gmail.com>
2016-10-01 19:42:59 +0000

    Adding security. Draft 1.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,15 +3,15 @@ class PagesController < ApplicationController
 require "twilio-ruby"
 
 def sms
-    
-account_sid = "AC97933738ad5d8fac72da857a2043bdd2" 
-auth_token = "8549d12985b7d27e0846364977c31a0c"
+  
+# account_sid = "AC97933738ad5d8fac72da857a2043bdd2"  
+# auth_token = "8549d12985b7d27e0846364977c31a0c"
 
     begin
-        @client = Twilio::REST::Client.new account_sid, auth_token
+        @client = Twilio::REST::Client.new ENV['TWILIO_ACCOUNT_SID'], ENV['TWILIO_AUTH_TOKEN']
         message = @client.account.messages.create(:body => "Here's your sms!",
         :to => params[:q],
-        :from => "+16315097017")  
+        :from => ENV['TWILIO_NUMBER'])  
     rescue Twilio::REST::RequestError => e
         puts e.message
     end 
