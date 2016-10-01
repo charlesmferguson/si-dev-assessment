ada54d852853f4205a20eed9b96fea0fd903b8af
Charles Ferguson <charles.m.ferguson.jr@gmail.com>
2016-10-01 18:55:13 +0000

    Finished the app, but do not have private variables in a different environment yet.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,8 +9,8 @@ auth_token = "8549d12985b7d27e0846364977c31a0c"
 
     begin
         @client = Twilio::REST::Client.new account_sid, auth_token
-        message = @client.account.messages.create(:body => "yello",
-        :to => :q,
+        message = @client.account.messages.create(:body => "Here's your sms!",
+        :to => params[:q],
         :from => "+16315097017")  
     rescue Twilio::REST::RequestError => e
         puts e.message
