c0ffe9ac01fdb0a81e17b9f877a208982845ac0e
Charles Ferguson <charles.m.ferguson.jr@gmail.com>
2016-10-01 20:14:44 +0000

    Adding security. Draft 2.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,14 +4,11 @@ require "twilio-ruby"
 
 def sms
   
-# account_sid = "AC97933738ad5d8fac72da857a2043bdd2"  
-# auth_token = "8549d12985b7d27e0846364977c31a0c"
-
     begin
-        @client = Twilio::REST::Client.new ENV['TWILIO_ACCOUNT_SID'], ENV['TWILIO_AUTH_TOKEN']
+        @client = Twilio::REST::Client.new ENV["account_sid"], ENV["auth_token"]
         message = @client.account.messages.create(:body => "Here's your sms!",
         :to => params[:q],
-        :from => ENV['TWILIO_NUMBER'])  
+        :from => ENV["number"])  
     rescue Twilio::REST::RequestError => e
         puts e.message
     end 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,7 +85,8 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 end
 
-
-TWILIO_ACCOUNT_SID = 'AC97933738ad5d8fac72da857a2043bdd2'
-TWILIO_AUTH_TOKEN = '8549d12985b7d27e0846364977c31a0c'
-TWILIO_NUMBER = '+16315097017'
+config.sms {
+account_sid = ENV["AC97933738ad5d8fac72da857a2043bdd2"]
+auth_token = ENV["8549d12985b7d27e0846364977c31a0c"]
+number = ENV["+16315097017"]
+}
